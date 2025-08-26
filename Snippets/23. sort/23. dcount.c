fefb78bdbc72231707244e43242ab78a887af60c
ismqdb <djukic.djukic96@gmail.com>
2025-08-26 21:46:28 +0200

    dist count

--- Snippets/23. sort/23. dcount.c
+++ Snippets/23. sort/23. dcount.c
@@ -25,3 +25,33 @@ void simpledc(i32 size){
 }
 
 /* ******************************************************************************** */
+
+// N records
+// keys: [0, M-1]
+
+void distrCount(i32 *array, i32 size, i32 maxKeys){
+    i32 count[maxKeys];
+    i32 idx[size];
+
+    for(i32 i = 0; i < maxKeys; i++){
+        count[i] = 0;
+    }
+
+    for(i32 i = 0; i < size; i++){
+        idx[i] = 0;
+    }
+
+    for(i32 i = 0; i < size; i++)
+        count[array[i]]++;
+
+    for(i32 i = 0; i < maxKeys-1; i++)
+        count[i] = count[i-1] + count[i];
+
+    for(i32 i = size-1; i > 0; i--)
+        idx[count[array[i]]--] = array[i];
+
+    for(i32 i = 0; i < size; i++)
+        array[i] = idx[i];
+}
+
+/* ******************************************************************************** */
